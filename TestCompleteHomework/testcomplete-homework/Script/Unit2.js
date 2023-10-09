8848a6d9d7773048a6804a083cf43a4c03a19afe
giorgidzindzibadze <giorgi.dzindzibadze.1@btu.edu.ge>
2023-10-10 00:22:38 +0400

    Add testcomplete-basics

---
+++ TestCompleteHomework/testcomplete-homework/Script/Unit2.js
@@ -0,0 +1,21 @@
+﻿function openingOrder(){
+  TestedApps.Orders.Run()
+}
+
+function AddOrder(){
+  KeywordTests.Test3.Run()
+  Aliases.Orders.MainForm.Activate();
+  Aliases.Orders.MainForm.MainMenu.Click("File|Save As...");
+  var randomValue = Math.floor(Math.random() * 10000000000000000) + 1;
+  var randomValueString = randomValue.toString();
+  Aliases.Orders.dlgSaveAs.DUIViewWndClassName.Explorer_Pane.FloatNotifySink.ComboBox.SetText(randomValueString);
+  Aliases.Orders.dlgSaveAs.btnSave.ClickButton();
+  
+}
+
+function closingOrder(){
+  TestedApps.Orders.Close()
+}
+module.exports.openingOrder = openingOrder;
+module.exports.AddOrder = AddOrder;
+module.exports.closingOrder = closingOrder;
---
+++ TestCompleteHomework/testcomplete-homework/Script/secondrun.js
@@ -0,0 +1,11 @@
+﻿var Task4 = require("Task4");
+
+function Main(){
+  Task4.openingOrder();
+  Task4.AddOrder1();
+  Task4.AddOrder2();
+  Task4.CheckOrder1();
+  Task4.checkOrder2();
+  Task4.deleteOrders();
+  Task4.closingOrder();
+}
