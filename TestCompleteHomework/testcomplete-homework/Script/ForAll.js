a5a13d50428cbeb216d674ef747fd5bf650f6fa7
giorgidzindzibadze <giorgi.dzindzibadze.1@btu.edu.ge>
2023-10-14 03:32:31 +0400

    Add testcomplete-patterns

---
+++ TestCompleteHomework/testcomplete-homework/Script/ForAll.js
@@ -0,0 +1,20 @@
+﻿function openingOrder(){
+  TestedApps.Orders.Run()
+ 
+}
+
+function closingOrder(){
+  TestedApps.Orders.Close();
+
+}
+
+function CustomAssert(actualValue, expectedValue) {
+    if (actualValue === expectedValue) {
+        Log.Message("Assertion Passed" );
+    } else {
+        Log.Error("Assertion Failed");
+    }
+}
+module.exports.CustomAssert = CustomAssert;
+module.exports.openingOrder = openingOrder;
+module.exports.closingOrder = closingOrder;
